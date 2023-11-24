bc4212179da6a8c7741926ddf34071fec45401de
KoPhaiHoang <thaihoangdang1803@gmail.com>
2023-11-24 15:40:06 +0700

    add drop down menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+"use client"
+
 import Link from "next/link";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 
 
 export default function Home() {
@@ -13,7 +16,7 @@ export default function Home() {
         </div>
         <div className="flex-1 flex">
           <div className="w-full my-auto flex border-2 border-neutral-200 rounded-full max-w-3xl mx-auto">
-            <input type="text" placeholder="Search for books.." className="w-full rounded-l-full border-r-2 border-neutral-200 h-12 px-5 outline-gray-300"/>
+            <input type="text" placeholder="Search for books.." className="h-12 w-full px-5 rounded-l-full border-r-2 border-neutral-200 outline-gray-300"/>
             <button className="w-20 lg:w-28 rounded-r-full bg-neutral-100 hover:bg-neutral-200 active:bg-neutral-300">
               <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" className="m-auto">
                 <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/>
@@ -21,23 +24,36 @@ export default function Home() {
             </button>
           </div>
         </div>
-        <div className="basis-2/12"></div>
+        <div className="basis-2/12 flex">
+          <div className="ml-auto flex">
+            <div className="my-auto mr-4 lg:mr-8">
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button className="bg-white hover:bg-white hover:border aspect-square active:bg-neutral-300 outline-none">
+                    <svg fill="none" color="black" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current">
+                      <path strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                    </svg>
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuItem className="text-md">Profile</DropdownMenuItem>
+                  <DropdownMenuItem className="text-md">Setting</DropdownMenuItem>
+                  <DropdownMenuItem className="text-md">Logout</DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          </div>
+        </div>
       </header>
       <div className="bg-white h-14 flex gap-2 border-b sticky top-0">
-        
+        <div className="">
+
+        </div>
       </div>
       <div className="h-96 bg-red-200"></div>
       <div className="h-96 bg-red-200"></div>
       <div className="h-96 bg-red-200"></div>
       <div className="px-8 lg:px-32 md:flex gap-5">
-        {/* <div className="w-full flex border-2 rounded-full">
-          <input type="text" placeholder="Search for courses" className="w-full rounded-l-full h-12 px-5  focus:outline-none"/>
-          <button className="bg-slate-400 w-16 rounded-r-full">
-            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
-              <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/>
-            </svg>
-          </button>
-        </div> */}
       </div>
     </div>
   );
