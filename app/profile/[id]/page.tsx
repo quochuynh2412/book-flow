156cd1e29a1d64c04e6fef53ef7b312043e9947d
KoPhaiHoang <thaihoangdang1803@gmail.com>
2023-12-28 19:35:30 +0700

    basic design

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -5,6 +5,8 @@ import { useState, useEffect } from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
+import bg1 from "@/public/img/bg1.png";
+
 
 export default function Page({ params }: { params: { id: string } }) {
   const [user, setUser] = useState(null);
@@ -26,9 +28,14 @@ export default function Page({ params }: { params: { id: string } }) {
   return (
     <div >
       <Header />
+      <div className="h-72 flex shadow-inner border bg-cover bg-no-repeat bg-center bg-blend-multiply bg-neutral-400" style={{ backgroundImage: `url(${bg1.src})` }}>
+        <div className="m-auto">
+          <p className="text-center font-light text-white mb-3">My Profile</p>
+          <h1 className="text-4xl md:text-6xl font-semibold text-white mb-3">{user && user["name"]}</h1>
+        </div>
+      </div>
       <div className="min-h-screen">
-        Hello 
-        {user && user["name"]}
+        
       </div>
       <Footer />
     </div>
