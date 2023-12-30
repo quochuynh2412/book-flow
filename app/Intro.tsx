7af96394dc54b56ee81d66554b82b6167d3a326c
Quoc Huynh <letrinhquoch@gmail.com>
2023-12-30 16:50:06 +0700

    BF-73 added countup effect

--- app/Intro.tsx
+++ app/Intro.tsx
@@ -1,12 +1,20 @@
+"use client"
+import { useRef, useEffect, useState } from "react";
 import vietnam from "@/public/vietnam.png";
+import CountUp from 'react-countup';
+import useCountUp from "react-countup";
 
 const stats = [
-  { id: 1, name: 'Books', value: '10,000+' },
-  { id: 2, name: 'Book reviews', value: '50,000+' },
-  { id: 3, name: 'New users annually', value: '5,000+' },
+  { id: 1, name: 'Books', value: 10000 },
+  { id: 2, name: 'Book reviews', value: 50000 },
+  { id: 3, name: 'New users annually', value: 5000 },
 ];
 
 export default function Intro() {
+  const countUpRef = useRef(null); // Add type annotation to countUpRef
+  // start counting when scroll to the element
+  const [countUp, setCountUp] = useState(false);
+
   return (
     <div id="intro" className="relative">
       <div className="sticky top-0 h-screen flex flex-col items-center justify-center bg-white">
@@ -16,7 +24,13 @@ export default function Intro() {
               <div key={stat.id} className="mx-auto flex max-w-xs flex-col gap-y-4">
                 <dt className="text-base leading-7 text-gray-600">{stat.name}</dt>
                 <dd className="order-first font-semibold tracking-tight text-3xl lg:text-6xl">
-                  {stat.value}
+                  <CountUp end={stat.value} // Add ref prop to CountUp component
+                    duration={2}
+                    decimals={0}
+                    decimal="."
+                    suffix="+"
+                    enableScrollSpy
+                    scrollSpyDelay={100} />
                 </dd>
               </div>
             ))}
@@ -24,10 +38,10 @@ export default function Intro() {
         </div>
       </div>
 
-      <div style={{backgroundImage: `url(${vietnam.src})`}} className="bg-cover bg-no-repeat bg-center sticky top-0 h-screen flex flex-col items-center justify-center text-white" >
+      <div style={{ backgroundImage: `url(${vietnam.src})` }} className="bg-cover bg-no-repeat bg-center sticky top-0 h-screen flex flex-col items-center justify-center text-white" >
         <div className="w-full">
-            <h1 className="mx-12 mb-10 text-center text-4xl lg:text-6xl">FOCUS ON THE <span className="text-yellow-400 font-bold">VIETNAMESE</span>!</h1>
-            <p className="mx-20 text-center text-lg">Built by Vietnamese people, for Vietnamese people.</p>
+          <h1 className="mx-12 mb-10 text-center text-4xl lg:text-6xl">FOCUS ON THE <span className="text-yellow-400 font-bold">VIETNAMESE</span>!</h1>
+          <p className="mx-20 text-center text-lg">Built by Vietnamese people, for Vietnamese people.</p>
         </div>
       </div>
     </div>
