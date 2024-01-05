08589164ce70b1934b4710197f36d3011d642a18
Quoc Huynh <letrinhquoch@gmail.com>
2024-01-05 14:09:08 +0700

    Add Firebase service account key file path resolution

--- lib/firebase-admin-config.ts
+++ lib/firebase-admin-config.ts
@@ -1,7 +1,9 @@
 import { initializeApp, getApps, cert } from 'firebase-admin/app';
+import key from './bookflow-e7dbd-firebase-adminsdk-vhk4z-eedc8960f3.json';
+import path from 'path';
 
 const firebaseAdminConfig = {
-    credential: cert(process.env.FIREBASE_SECRET_KEY?process.env.FIREBASE_SECRET_KEY:'')
+    credential: cert(path.resolve(__dirname, process.env.FIREBASE_SECRET_KEY||'')),
 }
 
 export function customInitApp() {
