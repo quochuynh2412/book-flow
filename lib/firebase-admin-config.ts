b2b62a590c7780e05066a5a754627c3399a20528
Quoc Huynh <letrinhquoch@gmail.com>
2024-01-05 14:23:14 +0700

    Update firebase-admin-config.ts

--- lib/firebase-admin-config.ts
+++ lib/firebase-admin-config.ts
@@ -1,9 +1,8 @@
 import { initializeApp, getApps, cert } from 'firebase-admin/app';
-import key from './bookflow-e7dbd-firebase-adminsdk-vhk4z-eedc8960f3.json';
 import path from 'path';
 
 const firebaseAdminConfig = {
-    credential: cert(path.resolve(__dirname, process.env.FIREBASE_SECRET_KEY||'')),
+    credential: cert(path.resolve(process.env.FIREBASE_SECRET_KEY || '')),
 }
 
 export function customInitApp() {
